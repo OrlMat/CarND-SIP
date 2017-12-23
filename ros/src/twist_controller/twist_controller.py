7030c9759bb84bf105fc97b61ea57c20e02c576c
MatOrl <m.orlowski74@gmail.com>
2017-12-23 14:33:44 +0100

    Twist controller refactoring - creation of two functions responsible for lateral and longitudinal control.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -27,41 +27,62 @@ class Controller(object):
             max_steer_angle=vehicle_params.max_steer_angle)
         
         self.vehicle_params = vehicle_params
-        self.pid = PID(kp, ki, kd, u_rising_rate, u_falling_rate, mn=vehicle_params.decel_limit, mx=vehicle_params.accel_limit)
+        self.pid = PID(kp, ki, kd, u_rising_rate, u_falling_rate,
+                       mn=vehicle_params.decel_limit, mx=vehicle_params.accel_limit)
         self.s_lpf = LowPassFilter(tau, ts)
         self.t_lpf = LowPassFilter(tau, ts)
 
     def control(self, twist_cmd, current_velocity, sample_time):
 
-        vehicle_mass = self.vehicle_params.vehicle_mass
-        fuel_mass = self.vehicle_params.fuel_capacity * GAS_DENSITY
-        wheel_radius = self.vehicle_params.wheel_radius
-        
+        # Rewrite and calculate basic car state
         linear_velocity = twist_cmd.twist.linear.x
         angular_velocity = twist_cmd.twist.angular.z
         current_linear_velocity = current_velocity.twist.linear.x
         velocity_error = linear_velocity - current_linear_velocity
-        print(linear_velocity)
 
-        get_steer = self.yaw_controller.get_steering(linear_velocity, 
-                                                     angular_velocity,
-                                                     current_linear_velocity)
+        # Lateral control
+        u_steering = self.lat_ctrl(linear_velocity, angular_velocity, current_linear_velocity)
 
-        steer = self.s_lpf.filt(get_steer)
+        # Longitudinal control
+        u_throttle, u_break = self.lon_ctrl(velocity_error, sample_time)
 
-        get_accel = self.pid.step(velocity_error, sample_time)
-        accel = self.t_lpf.filt(get_accel)
+        return u_throttle, u_break, u_steering
 
+    def lon_ctrl(self, velocity_error, sample_time):
+
+        # PID step
+        accel = self.pid.step(velocity_error, sample_time)
+
+        # Steering filtering
+        accel_filtered = self.t_lpf.filt(accel)
+
+        # Control logic
         if accel > 0.0:
-            throttle = accel
-            brake = 0.0
-        else:
-            throttle = 0.0
-            decel = -accel
-            
-            if decel < self.vehicle_params.brake_deadband:
-                decel = 0.0
+            # Throttle control
+            u_throttle = accel_filtered
+            u_brake = 0.0
 
-            brake = decel * wheel_radius * (vehicle_mass + fuel_mass)
- 
-        return throttle, brake, steer
+        else:
+            # Breaking control
+            u_throttle = 0.0
+            decel = -accel
+
+            # Check for too small deceleration to use breaks
+            if decel < self.vehicle_params.brake_deadband:
+                u_brake = 0.0
+            else:
+                u_brake = decel * self.vehicle_params.wheel_radius * (self.vehicle_params.vehicle_mass +
+                                                                      self.vehicle_params.fuel_capacity * GAS_DENSITY)
+
+        return u_throttle, u_brake
+
+    def lat_ctrl(self, linear_vel, angular_vel, linear_vel_curr):
+
+        # Derive control for steering
+        get_steer = self.yaw_controller.get_steering(linear_vel,
+                                                     angular_vel,
+                                                     linear_vel_curr)
+        # Filter control with use of low pass filter
+        steering_filtered = self.s_lpf.filt(get_steer)
+
+        return steering_filtered
